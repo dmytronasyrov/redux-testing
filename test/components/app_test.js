94aa7e31ca8b12cf859cbc9793ae10aa13b81377
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-04-02 19:44:20 +0300

    basics test specs

---
+++ test/components/app_test.js
@@ -0,0 +1,19 @@
+import { renderComponent, expect } from '../test_helper'
+import App from '../../src/components/App'
+
+describe('App', () => {
+
+  let component
+
+  beforeEach(() => {
+    component = renderComponent(App)
+  })
+
+  it('shows CommentBox', () => {
+    expect(component.find('.comment-box')).to.exist
+  })
+
+  it('shows CommentList', () => {
+    expect(component.find('.comment-list')).to.exist
+  })
+})

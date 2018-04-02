beed88a39b04b068919531eecf7740882ac03176
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-04-02 21:32:25 +0300

    add header

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Header from './Header'
 import CommentBox from './CommentBox'
 import CommentList from './CommentList'
 
@@ -7,8 +8,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <CommentList/>
-        <CommentBox/>
+        <Header/>
+        { this.props.children }
       </div>
     );
   }
